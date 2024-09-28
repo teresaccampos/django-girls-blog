7f0f11d8d86647502e92b456b2b3f1406a34e3d0
Teresa Campos <teresacampos42@gmail.com>
2024-09-28 18:51:42 -0300

    Testando static

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = '/home/teresacampos/teresacampos.pythonanywhere.com/static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
